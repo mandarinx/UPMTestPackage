75ab5de2e0848b4c2be0cfd1c8a5d77670f6c070
mandarinx <mandarin@mandarin.no>
2018-12-14 09:25:14 +0100

    Version 0.0.1

---
+++ TestPackage.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace UPMTestPackage {
+    public class TestPackage {
+        public static void Test() {
+            Debug.Log("Hello from UPM Test Package 0.0.1");
+        }
+    }
+}
