c95b846ecd9cdcdc4b3a2c826e526f10232e3ea0
mandarinx <mandarin@mandarin.no>
2018-12-14 09:52:22 +0100

    v0.0.2

--- TestPackage.cs
+++ TestPackage.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 namespace UPMTestPackage {
     public class TestPackage {
         public static void Test() {
-            Debug.Log("Hello from UPM Test Package 0.0.1");
+            Debug.Log("Hello from UPM Test Package 0.0.2");
         }
     }
 }
